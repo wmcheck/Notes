8d26bf0e2069602b2086c2b1e6a838e53b1fb207
Илясов Алексей <ilasov.alexey@gmail.com>
2023-12-11 15:22:21 +0300

    Update sample.js

--- task/sample.js
+++ task/sample.js
@@ -9,5 +9,20 @@
     f[8]();
 })();
 
+
 //100
 //100
+
+
+(function() { 
+    var f = []; 
+    for (let i = 0; i < 100; i++) { 
+        f[i] = () => console.log(i); 
+    }
+    console.log(f);
+    f[9]();
+    f[8]();
+})();
+
+//9
+//8
